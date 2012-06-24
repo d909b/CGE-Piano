db61b1dd4150efae0ff58de8a511b446fafb18e9
Patrick Schwab <patrick.schwab@technikum-wien.at>
2012-06-24 12:39:57 +0200

    Initial commit

---
+++ Src/GLFWWrapper.cpp
@@ -0,0 +1,33 @@
+/*
+ * GLFWWrapper.cpp
+ *
+ * @date: Jun 22, 2012
+ * @author: Patrick Schwab
+ */
+
+#include "GLFWWrapper.h"
+
+GLFWWrapper::GLFWWrapper()
+{
+	;
+}
+
+GLFWWrapper::~GLFWWrapper()
+{
+	;
+}
+
+void GLFWWrapper::initialize() const
+{
+}
+
+void GLFWWrapper::terminate() const
+{
+}
+
+void GLFWWrapper::openWindow(int width, int height, int redbits, int greenbits,
+						     int bluebits, int alphabits, int depthbits, int stencilbits,
+						     int mode) const
+{
+}
+
